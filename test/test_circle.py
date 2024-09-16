1abe286d1f14af62e1f2e486063bae296cc3c73f
Galina Sedreeva <gsedreeva@s-terra.ru>
2024-09-16 17:04:55 +0300

    negative, parametrised and add_aera tests added

--- test/test_circle.py
+++ test/test_circle.py
@@ -1,10 +1,4 @@
-import os
-import sys
-
-sys.path.append("src")
-print(os.getcwd())
-
-
+from src.rectangle import Rectangle
 from src.circle import Circle
 import pytest
 
@@ -25,17 +19,67 @@ def before_circle_area():
 
 def test_circle_perimeter(prepare_to_det):
     cir1 = Circle(1)
-    assert cir1.perimeter == 6, f"Длина окружности с радиусом 1 должна быть равна 6"
+    assert cir1.perimeter == 6, "Длина окружности с радиусом 1 должна быть равна 6"
 
 
-def test_circle_area(before_circle_area):
-    cir2 = Circle(2)
-    assert cir2.area == 13, f"Площадь круга радиусом 2 должна быть равна 13"
+@pytest.mark.parametrize(
+    "a, area",
+    [
+        (12, 452),
+        pytest.param(5, 79, marks=pytest.mark.smoke),
+        (1.73, 9),
+    ],
+    ids=[
+        "integer",
+        "smoke",
+        "real",
+    ],
+)
+def test_circle_area(a, area):
+    cir2 = Circle(a)
+    assert cir2.area == area, f"Площадь круга радиусом {a} должна быть равна {area}"
 
 
-def test_circle_add():
-    cir3 = Circle(3)
-    cir4 = Circle(4)
+@pytest.mark.parametrize(
+    "a, b, sum_area",
+    [
+        (12, 48, 7690),
+        pytest.param(5, 20, 1336, marks=pytest.mark.smoke),
+        (1.73, 4.5, 73),
+    ],
+    ids=[
+        "integer",
+        "smoke",
+        "real",
+    ],
+)
+def test_circle_add_circle(a, b, sum_area):
+    cir3 = Circle(a)
+    cir4 = Circle(b)
     assert (
-        cir3.add_area(cir4) == 78
-    ), f"Площадь двух окружностей с радиусами 3 и 4 должна быть равна 78"
+        cir3.add_area(cir4) == sum_area
+    ), f"Площадь двух окружностей с радиусами {a} и {b} должна быть равна {sum_area}"
+
+
+@pytest.mark.parametrize(
+    "s, a, b, sum_area",
+    [
+        (13, 5, 20, 631),
+        pytest.param(11, 3, 15, 425, marks=pytest.mark.smoke),
+        (0.5, 1.5, 2.6, 4.9),
+    ],
+    ids=[
+        "integer",
+        "smoke",
+        "real",
+    ],
+)
+def test_circle_add_rectangle(s, a, b, sum_area):
+    cir5 = Circle(s)
+    rec5 = Rectangle(a, b)
+    assert cir5.add_area(rec5) == sum_area, f"Sum sq {a} and {b} must be eq {sum_area}"
+
+
+def test_circle_negative():
+    with pytest.raises(ValueError, match="Радиус не может быть <=0"):
+        cir5 = Circle(-1)
--- test/test_rectangle.py
+++ test/test_rectangle.py
@@ -1,23 +1,49 @@
-import os
-import sys
-
-sys.path.append("src")
-print(os.getcwd())
-
 from src.rectangle import Rectangle
+import pytest
 
 
-def test_rec_perimeter():
-    rec1 = Rectangle(10, 20)
-    assert rec1.perimeter == 60
+@pytest.mark.parametrize(
+    "a, b, perimeter",
+    [
+        (12, 4, 32),
+        pytest.param(5, 7, 24, marks=pytest.mark.smoke),
+        (1.73, 2.54, 8.54),
+    ],
+    ids=[
+        "integer",
+        "smoke",
+        "real",
+    ],
+)
+def test_rec_perimeter(a, b, perimeter):
+    rec1 = Rectangle(a, b)
+    assert rec1.perimeter == perimeter, f"perimeter should be eq {perimeter}"
 
 
-def test_rec_area():
-    rec2 = Rectangle(2, 4)
-    assert rec2.area == 8
+@pytest.mark.parametrize(
+    "a, b, area",
+    [
+        (12, 4, 48),
+        pytest.param(5, 7, 35, marks=pytest.mark.smoke),
+        (1.73, 2.54, 4.3942),
+    ],
+    ids=[
+        "integer",
+        "smoke",
+        "real",
+    ],
+)
+def test_rec_area(a, b, area):
+    rec2 = Rectangle(a, b)
+    assert rec2.area == area, f"area should be eq {area}"
 
 
 def test_rec_add():
     rec3 = Rectangle(5, 6)
     rec4 = Rectangle(7, 8)
-    assert rec3.add_area(rec4) == 86
+    assert rec3.add_area(rec4) == 86, "area should be eq 86"
+
+
+def test_rectangle_negative():
+    with pytest.raises(ValueError, match="Длина стороны не может быть <=0"):
+        rec = Rectangle(0, 1)
